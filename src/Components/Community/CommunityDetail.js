13faabf2cdd877a69a526ef214f3e36bcb45dbbb
niloaydin <nilay.aydn97@gmail.com>
2024-05-19 16:43:49 +0300

    implement advanced search

--- src/Components/Community/CommunityDetail.js
+++ src/Components/Community/CommunityDetail.js
@@ -11,6 +11,10 @@ import {
   CardContent,
   Button,
   TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import MembersModal from "./MembersListDialog";
 import TemplateListDialog from "./TemplateDialog";
@@ -28,15 +32,17 @@ const CommunityDetail = () => {
   const [members, setMembers] = useState([]);
   const [isOwner, setIsOwner] = useState(false);
   const [templates, setTemplates] = useState([]);
+  const [selectedTemplate, setSelectedTemplate] = useState("");
+  const [fieldValues, setFieldValues] = useState({});
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [openMembersModal, setOpenMembersModal] = useState(false);
+  const [openTemplateModal, setOpenTemplateModal] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isAdvancedSearchActive, setIsAdvancedSearchActive] = useState(false);
+
   const token = localStorage.getItem("token");
 
   const email = localStorage.getItem("userEmail");
-
-  const [openMembersModal, setOpenMembersModal] = useState(false);
-  const [openTemplateModal, setOpenTemplateModal] = useState(false);
-  const [openCreatePostTemplateModal, setOpenCreatePostTemplateModal] =
-    useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetchCommunityDetails(id);
@@ -276,6 +282,49 @@ const CommunityDetail = () => {
     }
   };
 
+  const handleTemplateChange = (event) => {
+    setSelectedTemplate(event.target.value);
+    setFieldValues({});
+    setFilteredPosts([]);
+  };
+
+  const handleFieldValueChange = (fieldName, value) => {
+    setFieldValues({ ...fieldValues, [fieldName]: value });
+  };
+
+  const handleSubmitForAdvancedSearch = async () => {
+    try {
+      const queryParams = {
+        template: selectedTemplate,
+        ...fieldValues,
+      };
+
+      const queryString = new URLSearchParams(queryParams).toString();
+
+      const response = await fetch(
+        `${API_URL}/${id}/searchPosts?${queryString}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setFilteredPosts(data);
+        if (data.length === 0) {
+          alert("There is no posts with this requirements!");
+        }
+      } else {
+        throw new Error(await response.text());
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   const isUrl = (value) => {
     const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
     return urlPattern.test(value);
@@ -306,7 +355,7 @@ const CommunityDetail = () => {
     setSearchQuery(event.target.value);
   };
 
-  const filteredPosts = posts.filter((post) => {
+  const basicSearchPosts = posts.filter((post) => {
     const search = searchQuery.toLowerCase();
     return Object.keys(post.content).some((key) =>
       post.content[key].toLowerCase().includes(search)
@@ -361,13 +410,21 @@ const CommunityDetail = () => {
         <Box>
           {isOwner && (
             <Link to={`/community/${id}/create-template`}>
-              <Button variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ marginRight: "10px", marginTop: "10px" }}
+              >
                 Create Post Template
               </Button>
             </Link>
           )}
           <Link to={`/community/${id}/create-post`}>
-            <Button variant="contained" color="primary">
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ marginRight: "10px", marginTop: "10px" }}
+            >
               Create Post
             </Button>
           </Link>
@@ -375,7 +432,7 @@ const CommunityDetail = () => {
             variant="contained"
             color="secondary"
             onClick={handleSeeMembers}
-            sx={{ marginRight: "10px" }}
+            sx={{ marginRight: "10px", marginTop: "10px" }}
           >
             See Members
           </Button>
@@ -383,38 +440,128 @@ const CommunityDetail = () => {
             variant="contained"
             color="secondary"
             onClick={handleSeeTemplates}
-            sx={{ marginRight: "10px" }}
+            sx={{ marginRight: "10px", marginTop: "10px" }}
           >
             See Post Templates
           </Button>
+          <Button
+            variant="contained"
+            color={isAdvancedSearchActive ? "error" : "primary"}
+            sx={{ marginRight: "10px", marginTop: "10px" }}
+            onClick={() => setIsAdvancedSearchActive(!isAdvancedSearchActive)}
+          >
+            {!isAdvancedSearchActive
+              ? "Advanced Search"
+              : "Close Advanced Search"}
+          </Button>
         </Box>
       </Container>
-      <Container sx={{ marginTop: "20px" }}>
-        <Box sx={{ marginTop: "20px", display: "flex", flexWrap: "wrap" }}>
-          {filteredPosts.map((post, index) => (
-            <Card key={post.id} sx={{ width: "100%", marginBottom: "20px" }}>
-              <CardContent>
-                <Typography variant="body2" color="textSecondary">
-                  posted by: {post.created_by.username}
-                </Typography>
-                {Object.keys(post.content).map((key) => (
-                  <Typography key={key} color="text.secondary">
-                    <strong>{key}: </strong>
-                    {renderFieldValue(post.content[key])}
-                  </Typography>
+      {isAdvancedSearchActive ? (
+        <div>
+          <Container>
+            <Typography variant="h6" gutterBottom>
+              Advanced Search
+            </Typography>
+            <FormControl fullWidth>
+              <InputLabel>Select Template</InputLabel>
+              <Select value={selectedTemplate} onChange={handleTemplateChange}>
+                <MenuItem value="">Select Template</MenuItem>
+                {templates.map((template) => (
+                  <MenuItem key={template.id} value={template.id}>
+                    {template.name}
+                  </MenuItem>
                 ))}
+              </Select>
+            </FormControl>
+
+            {selectedTemplate && (
+              <div>
+                {templates
+                  .find((template) => template.id === selectedTemplate)
+                  ?.datafields.map((field) => (
+                    <Box
+                      key={field.id}
+                      sx={{ marginBottom: "10px", marginTop: "20px" }}
+                    >
+                      <TextField
+                        label={field.name}
+                        style={{ width: "100%" }}
+                        value={fieldValues[field.name] || ""}
+                        type={field.type.toLowerCase()}
+                        onChange={(e) =>
+                          handleFieldValueChange(field.name, e.target.value)
+                        }
+                      />
+                    </Box>
+                  ))}
                 <Button
                   variant="contained"
-                  color="error"
-                  onClick={() => handleDeletePost(post.id)}
+                  color="primary"
+                  onClick={handleSubmitForAdvancedSearch}
                 >
-                  Delete
+                  Search
                 </Button>
-              </CardContent>
-            </Card>
-          ))}
-        </Box>
-      </Container>
+              </div>
+            )}
+          </Container>
+          <Container sx={{ marginTop: "20px" }}>
+            <Box sx={{ marginTop: "20px", display: "flex", flexWrap: "wrap" }}>
+              {filteredPosts.map((post, index) => (
+                <Card
+                  key={post.id}
+                  sx={{ width: "100%", marginBottom: "20px" }}
+                >
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary">
+                      posted by: {post.created_by.username}
+                    </Typography>
+                    {Object.keys(post.content).map((key) => (
+                      <Typography key={key} color="text.secondary">
+                        <strong>{key}: </strong>
+                        {renderFieldValue(post.content[key])}
+                      </Typography>
+                    ))}
+                    <Button
+                      variant="contained"
+                      color="error"
+                      onClick={() => handleDeletePost(post.id)}
+                    >
+                      Delete
+                    </Button>
+                  </CardContent>
+                </Card>
+              ))}
+            </Box>
+          </Container>
+        </div>
+      ) : (
+        <Container sx={{ marginTop: "20px" }}>
+          <Box sx={{ marginTop: "20px", display: "flex", flexWrap: "wrap" }}>
+            {basicSearchPosts.map((post, index) => (
+              <Card key={post.id} sx={{ width: "100%", marginBottom: "20px" }}>
+                <CardContent>
+                  <Typography variant="body2" color="textSecondary">
+                    posted by: {post.created_by.username}
+                  </Typography>
+                  {Object.keys(post.content).map((key) => (
+                    <Typography key={key} color="text.secondary">
+                      <strong>{key}: </strong>
+                      {renderFieldValue(post.content[key])}
+                    </Typography>
+                  ))}
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={() => handleDeletePost(post.id)}
+                  >
+                    Delete
+                  </Button>
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
+        </Container>
+      )}
       {openMembersModal && (
         <MembersModal
           open={openMembersModal}
